417bbc149a43be2fe068641d751ce77b733c0a21
paigetech <paigetech@gmail.com>
2016-02-16 13:47:59 -0800

    heroku database address

--- config/database.js
+++ config/database.js
@@ -2,6 +2,6 @@ module.exports = {
   // the database url to connect
   // local
   //url : 'mongodb://127.0.0.1:27017/alliance'
-  // heroku
+  // heroku address
   url: 'mongodb://heroku_svdkzvxb:951rcl4i1d7gq0kes5mtabcpci@ds011268.mongolab.com:11268/heroku_svdkzvxb'
 }
