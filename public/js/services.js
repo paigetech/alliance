b9036a0af1b145fa3aa3cd4558fb3c566e0ccd36
paigetech <paigetech@gmail.com>
2016-11-27 19:48:56 -0800

    add auth serivce to angular

--- public/js/services.js
+++ public/js/services.js
@@ -1,19 +1,83 @@
-angular.module('alliance').factory('AuthService', ['$http', function($http){
-  function isLoggedIn() {
-    // check if user is loggedin
-    $http.get("/loggedin")
-    .success(function (data) {
-      if (data.isLoggedIn) {
-        // if user is logged in update the global obj
-        User = data;
-        // create a local copy of the global obj for local access
-        return true;
+angular.module('alliance')
+.factory('Auth', ['$resource', '$rootScope', '$sessionStorage', '$q',
+  function($resource, $rootScope, $sessionStorage, $q){
+
+    var Profile = $resource('/api/profile', {}, {
+      getUser: {
+        method: "GET"
       }
-    })
-    .error(function (err) {
-      console.log('Error: ' + err);
-      return false;
     });
-  };
+
+    var auth = {};
+
+    auth.init = function(){
+      if(auth.isLoggedIn()){
+        $rootScope.user = currentUser();
+      } else {
+        auth.getUser();
+      }
+      //setup the user here?
+    };
+
+    auth.getUser = function(username, password){
+      return $q(function(resolve, reject){
+          Profile.getUser().$promise
+          .then(function(data) {
+              $sessionStorage.user = data;
+              $rootScope.user = $sessionStorage.user;
+              resolve();
+          }, function() {
+              reject();
+          });
+      });
+    };
+
+    auth.checkPermissionForView = function(view) {
+       if (!view.requiresAuthentication) {
+           return true;
+       }
+
+       return userHasPermissionForView(view);
+    };
+
+
+    var userHasPermissionForView = function(view){
+       if(!auth.isLoggedIn()){
+           return false;
+       }
+
+       if(!view.permissions || !view.permissions.length){
+           return true;
+       }
+
+       return auth.userHasPermission(view.permissions);
+    };
+
+
+    auth.userHasPermission = function(permissions){
+       if(!auth.isLoggedIn()){
+           return false;
+       }
+
+       var found = false;
+       angular.forEach(permissions, function(permission, index){
+           if ($sessionStorage.user.user_permissions.indexOf(permission) >= 0){
+               found = true;
+               return;
+           }
+       });
+
+       return found;
+    };
+
+    auth.currentUser = function(){
+      return $sessionStorage.user;
+    };
+
+    auth.isLoggedIn = function(){
+      return $sessionStorage.user !== null;
+    };
+
+    return auth;
 
 }]);
